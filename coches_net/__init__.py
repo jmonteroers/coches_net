b5d06eae25798b557a0cb62679f27e3f72d7297a
Juan Montero <jmonteroers@gmail.com>
2020-10-18 20:02:01 +0200

    modify project to have a package structure

---
+++ coches_net/__init__.py
@@ -0,0 +1,2 @@
+from .ingest import get_all_cars, add_new_pages
+from .residual_analysis import find_discounts
--- coches_net/ingest.py
+++ coches_net/ingest.py
@@ -1,15 +1,7 @@
-import os.path
 import pandas as pd
+import os.path
 from read_page import from_html_to_df
 import shutil
-
-# an ad-hoc exception to raise if some of the ad pages have not been processed
-class AdNotProcessedException(Exception):
-    def __init__():
-        exception_msg = \
-        ('A problem has ocurred when extracting one of the'
-         'dataframes. stopping execution')
-        super().__init__(exception_msg)
 
 
 def get_all_cars(source_dir: str, *, filename=None: str,
@@ -79,6 +71,50 @@ def get_all_cars(source_dir: str, *, filename=None: str,
             cars.to_csv(os.path.join(save_dir, f'{filename}.pkl', index=False)
     return cars
 
-if __name__ == '__main__':
-    # to-do: fill with example
-    pass
+
+def add_new_pages(source_dir: str,
+                  datafile: str, *,
+                  date_download: datetime.datetime=None,
+                  dest_dir: str=None,
+                  check_all_pages: bool=False,
+                  save=False):
+    '''
+    Processes all html files in `source_dir` with ads from coches.net,
+    and concatenates them into the existing dataframe in datafile. It always
+    returns the resulting dataframe. Optionally, it can rewrite the existing
+    dataframe by setting save to True.
+
+    Values:
+    - source_dir, folder from which to process all html files. does not
+    process subfolders
+    - datafile, relative/absolute path to location where existing dataframe exists
+    - dest_dir, directory to move original html files to. If None, do not move
+    original files
+    - date_download, date when html pages were obtained, to add it to dataframe.
+    if None, uses today as date_download
+    - check_all_pages, whether execution should be stopped if one of the pages
+    cannot be processed. if False, print a warning to the screen.
+    - save, should the original dataframe with the new observations
+    be saved (True) or returned (False)?
+    '''
+    if datafile.endswith('.pkl'):
+        df = pd.read_pickle(datafile)
+    elif datafile.endswith('.csv'):
+        df = pd.read_csv(datafile)
+    else:
+        print('datafile must be either a csv or a pickle file')
+        return None
+    n_initial_obs = df.shape[0]
+    new_car_ads = get_all_cars(source_dir, date_download=date_download,
+                               dest_dir=dest_dir,
+                               check_all_pages=check_all_pages)
+    # need to check that new_car_ads is not None
+    if new_car_ads:
+        df = pd.concat([df, new_car_ads])
+    n_final_obs = df.shape[0]
+    print(f'{n_final_obs - n_initial_obs} new cars have been added.')
+    if save and datafile.endswith('.pkl'):
+        df.to_pickle(datafile)
+    elif save and datafile.endswith('.csv'):
+        df.to_csv(datafile)
+    return df
