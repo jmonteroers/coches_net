a137f90de64549277a1c3d11dce585701da102c2
Juan Montero <jmonteroers@gmail.com>
2020-10-19 23:25:30 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='coches_net',
+    version='0.1',
+    packages=['coches_net'],
+    license='MIT',
+    author='Juan Antonio Montero de Espinosa',
+    description='functions to read and find discounts from coches.net'
+)
